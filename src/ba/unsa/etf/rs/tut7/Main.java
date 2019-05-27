43df50cf35a430ae5e6b526534da96cd67606014
HARUN <harun.hadzic17@gmail.com>
2019-05-27 14:35:51 +0200

    Implementacija JavaStreamApi-ja

--- src/ba/unsa/etf/rs/tut7/Main.java
+++ src/ba/unsa/etf/rs/tut7/Main.java
@@ -1,19 +1,50 @@
 package ba.unsa.etf.rs.tut7;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // Creating a list of integers
-        List<Integer> list = Arrays.asList(-9, -18, 0, 25, 4);
+        ArrayList<Integer> niz = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int tok;
 
-        System.out.println("The sorted stream is : ");
+        Comparator comparator = new Comparator<Integer>() {
+            @Override
+            public int compare(Integer br1, Integer br2) {
+                int sumaCifara1 = 0;
+                while (br1 != 0) {
+                    sumaCifara1 += br1 % 10;
+                    br1 /= 10;
+                }
+                int sumaCifara2 = 0;
+                while (br2 != 0) {
+                    sumaCifara2 += br2 % 10;
+                    br2 /= 10;
+                }
+                return sumaCifara1 - sumaCifara2;
+            }
+        };
 
-        // displaying the stream with elements
-        // sorted in natural order
-        list.stream().sorted().forEach(System.out::println);
+        niz.clear();
+        System.out.println("Unesite niz brojeva: ");
+        tok = scanner.nextInt();
+        while (tok != 0) {
+            niz.add(tok);
+            tok = scanner.nextInt();
+        }
+        System.out.println();
+        ArrayList<Integer> arrayListStreamAPI = (ArrayList<Integer>) niz.stream().sorted(comparator).collect(Collectors.toList());
+        System.out.println("Sortiran niz uz pomoc java stream API-ja glasi: ");
+        for(int i: arrayListStreamAPI) {
+            System.out.print(i + " ");
+        }
+
+
     }
+
+
+
 }
